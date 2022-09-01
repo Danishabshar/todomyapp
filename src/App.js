5cbadf6642167f5a8153c12b36336f85922436dc
Danish Abshar <dabshar4@gmail.com>
2022-09-01 09:17:30 +0530

    todoapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
 import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
 
 function App() {
+const[todos,settodos]=useState([])
+const [todo, setTodo] = useState("");
+const [todoediting,settodoediting] =useState(null);
+const [edittext,setedittext]=useState("")
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+
+  const newtodo= {
+    id: new Date().getTime(),
+    text : todo,
+    completed: false,
+  }
+
+  settodos([...todos].concat(newtodo))
+  setTodo("")
+
+}
+
+
+const Deletetodo=(id)=>{
+  const updatedtodo=[...todos].filter((todo)=> todo.id !== id)
+  settodos(updatedtodo)
+}
+
+
+
+const edittodo=(id)=>{
+  const updatedtodo=[...todos].map((todo)=>{
+    if(todo.id===id){
+      todo.text=edittext
+    }
+    return todo
+  })
+  settodos(updatedtodo)
+  settodoediting(null)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Todo-list</h1>
+      <form onSubmit={handleSubmit} >
+        {/* <input type="text" onChange={(e)=>setTodo(e.target.value) } value={todo}  /> */}
+
+        <input
+            type="text"
+            value={todo}
+            onChange={(e) => { 
+              setTodo(e.target.value)
+              console.log(e.target.value)
+              }}
+              />
+        <button type="submit">Add Todo</button>
+      </form>
+    {todos.map((todo)=>
+    <div key={todo.id}>
+    
+    {todoediting===todo.id ? ( <input type="text"
+     onChange={(e)=> setedittext(e.target.value)}
+     value={edittext}
+
+    />) : (<div>{todo.text}</div>)}
+
+   <button onClick={()=> Deletetodo(todo.id)}>DElete</button>
+
+   {todoediting===todo.id ? (<button onClick={()=>edittodo(todo.id)}>submit</button>):( <button onClick={()=>settodoediting(todo.id)}>edit</button>)}
+  
+
+
     </div>
+    
+    
+    )}
+    </div>
+    
+    
+
+   
   );
 }
 
